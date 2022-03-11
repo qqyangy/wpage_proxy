0ef4da9150c9034048be1c687fb080030b20fb34
yong.yang <yong.yang@17zuoye.com>
2022-03-11 16:08:21 +0800

    变量更名

--- utils.js
+++ utils.js
@@ -1,28 +1,28 @@
-function formatReqCfg(req,url,results){
+function formatResCfg(res,url,results){
   return ({
-    "[object Function]":()=>results.push(req),
-    "[object Array]":()=>req.forEach(o=>formatReqCfg(o,url,results)),
+    "[object Function]":()=>results.push(res),
+    "[object Array]":()=>res.forEach(o=>formatResCfg(o,url,results)),
     "[object Object]":()=>{
-      if("test" in req){
-        const type=Object.prototype.toString.call(req.test),
-        sTest=type==="[object String]" && url.includes(req.test),
-        rTest=type==="[object RegExp]" && req.test.test(url);
+      if("test" in res){
+        const type=Object.prototype.toString.call(res.test),
+        sTest=type==="[object String]" && url.includes(res.test),
+        rTest=type==="[object RegExp]" && res.test.test(url);
         if(!sTest && !rTest) return;//有test且验证不通过
       }
-      const headers=req.headers&&req.headers.constructor===Object&&req.headers; //获取配置的headers
-      if(req.body && req.body.constructor===Object){
+      const headers=res.headers&&res.headers.constructor===Object&&res.headers; //获取配置的headers
+      if(res.body && res.body.constructor===Object){
         // 需要使用mock数据
         results.mockIndex=results.length;
         results.push(function(){
           headers&&(this.headers=headers);//判断是否需要设置headers
-          this.body=req.body;
+          this.body=res.body;
         });
-      }else if(req.handler && req.handler.constructor===Function){
+      }else if(res.handler && res.handler.constructor===Function){
         // 使用handler处理
         results.push(headers?function(...p){
           this.headers=headers;
-          return req.handler.call(this,...p);
-        }:req.handler);
+          return res.handler.call(this,...p);
+        }:res.handler);
       }else if(headers){
         // 只有headers时生成处理函数
         return  results.push(function(){
@@ -30,7 +30,7 @@ function formatReqCfg(req,url,results){
         })
       }
     }
-  }[Object.prototype.toString.call(req)]||(()=>{}))()
+  }[Object.prototype.toString.call(res)]||(()=>{}))()
 }
 module.exports={
   // 格式化url
@@ -55,8 +55,8 @@ module.exports={
   extractRes(pcfg,cfg,env){
     const url=env.url;
     const resultfuncs=[];
-    formatReqCfg(pcfg.req,url,resultfuncs); // 处理根配置
-    formatReqCfg(cfg.req,url,resultfuncs); // 处理对应域配置
+    formatResCfg(pcfg.res,url,resultfuncs); // 处理根配置
+    formatResCfg(cfg.res,url,resultfuncs); // 处理对应域配置
     const mock="mockIndex" in resultfuncs;//是否为mock类型
     return {
       mock,
