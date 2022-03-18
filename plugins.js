bd7d10febb4adf1a2f758c12fc160178a53abe60
yong.yang <yong.yang@17zuoye.com>
2022-03-18 17:08:18 +0800

    plugins代码调整

--- plugins.js
+++ plugins.js
@@ -24,7 +24,6 @@ insertScript=(html,keys=[],obj,endmark)=>{
   const nhtml=html.replace(/<script\b[\W\w]*<\/script>/g,t=>{
     const slist=[];
     i++;
-    console.log(keys[0],keys[1]);
     while(keys.length>0 && keys[0]%100<=i){
       const k=keys.shift(),
       t=formatScript(obj[k]);
@@ -36,50 +35,50 @@ insertScript=(html,keys=[],obj,endmark)=>{
   });
   const lastScripts=keys.map(k=>formatScript(obj[k])).filter(t=>t).join("\n");//需要插入尾部的script标签
   return lastScripts?nhtml.replace(endmark,`${lastScripts}${endmark}`):nhtml;
+},
+//过滤加工url
+filterUrl=()=>{
+  return (url="")=>{
+    const index=hosts.findIndex(d=>url.includes(d.host));
+    return index!==-1?url.replace(hosts[index].host,`${location.protocol}//${location.hostname}:${hosts[index].serverPort}`):url;
+  }
+},
+//设置xhr
+xhrTxt=()=>{
+  return ()=>{
+    const myfetch=window.fetch,
+    myopen=XMLHttpRequest.prototype.open;
+    window.fetch=function fetch(url,...p){
+      return myfetch(filterUrl(url),...p);
+    }
+    XMLHttpRequest.prototype.open=function open(type,url,...p){
+      return myopen.call(this,type,filterUrl(url),...p);
+    }
+  }
+},
+//设置css与js
+cssAndJstxt=()=>{
+  return ()=>{
+    const appendChild2=HTMLElement.prototype.appendChild;
+    HTMLElement.prototype.appendChild=function appendChild(dom,...p){
+      if(dom instanceof HTMLScriptElement && dom.src){
+        dom.src=filterUrl(dom.src);
+      }else if(dom instanceof HTMLStyleElement && dom.href){
+        dom.href=filterUrl(dom.href);
+      }
+      return appendChild2.call(this,dom,...p);
+    }
+  }
 };
 const plugins={
   //插入代码
   insertInnerScript(html="",h,{hosts}){
     return html.replace("<head>",`<head><script>(()=>{
       const hosts=${JSON.stringify(hosts)},
-      filterUrl=${plugins.filterUrl().toString()};
-      (${plugins.xhrTxt().toString()})();
-      (${plugins.cssAndJstxt().toString()})();
+      filterUrl=${filterUrl().toString()};
+      (${xhrTxt().toString()})();
+      (${cssAndJstxt().toString()})();
     })()</script>`);
-  },
-  //过滤加工url
-  filterUrl(){
-    return (url="")=>{
-      const index=hosts.findIndex(d=>url.includes(d.host));
-      return index!==-1?url.replace(hosts[index].host,`${location.protocol}//${location.hostname}:${hosts[index].serverPort}`):url;
-    }
-  },
-  //设置xhr
-  xhrTxt(){
-    return ()=>{
-      const myfetch=window.fetch,
-      myopen=XMLHttpRequest.prototype.open;
-      window.fetch=function fetch(url,...p){
-        return myfetch(filterUrl(url),...p);
-      }
-      XMLHttpRequest.prototype.open=function open(type,url,...p){
-        return myopen.call(this,type,filterUrl(url),...p);
-      }
-    }
-  },
-  //设置css与js
-  cssAndJstxt(){
-    return ()=>{
-      const appendChild2=HTMLElement.prototype.appendChild;
-      HTMLElement.prototype.appendChild=function appendChild(dom,...p){
-        if(dom instanceof HTMLScriptElement && dom.src){
-          dom.src=filterUrl(dom.src);
-        }else if(dom instanceof HTMLStyleElement && dom.href){
-          dom.href=filterUrl(dom.href);
-        }
-        return appendChild2.call(this,dom,...p);
-      }
-    }
   },
   // 处理配置项的module字段
   moduleCode(text,h,{hosts,hostName,oldOrigin,contentType,url}){
