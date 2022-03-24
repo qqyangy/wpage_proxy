bf6e7b201c616d51d8fe2ed7219a7fec2dd2cb77
yong.yang <yong.yang@17zuoye.com>
2022-03-24 11:02:57 +0800

    程序允许传递工作目录

--- bin/wpage_proxy.sh
+++ bin/wpage_proxy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# 判断是否传入目录参数 如果有则切换到指定目录
+runpath=${1%proxy.config.js*};
+if [[ "$runpath" != "" && -f "${runpath%/}/proxy.config.js" ]];then
+  cd $runpath;
+fi;
 # 运行代理程序
-node /Users/admin/project/wpage_proxy/main.js
+node /Users/admin/project/wpage_proxy/main.js;
